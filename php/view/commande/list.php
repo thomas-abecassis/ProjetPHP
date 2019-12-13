a2f0f715b1b54aa69052a51d1bc0723e2c15b63e
Thomas Abecassis <thomas.abecassis@etu.umontpellier.fr>
2019-12-13 13:48:48 +0100

    derniers bug

--- php/view/commande/list.php
+++ php/view/commande/list.php
@@ -2,7 +2,7 @@
         <?php
         if(isset($tabCommande) && !empty($tabCommande)){
         foreach ($tabCommande as $commande){
-            echo "<a href=index.php?controller=commande&action=Read&id=".rawurlencode($commande->getId())."&prix=".rawurlencode($commande->getPrix())."&date=".$commande->getDate()."> commande du ".$commande->getDate() ." au prix de : ".rawurlencode($commande->getPrix()). " status : ".$commande->getStatus()."</a> <br>";
+            echo "<a href=index.php?controller=commande&action=Read&id=".rawurlencode($commande->getId())."&prix=".rawurlencode($commande->getPrix())."&date=".$commande->getDate()."> commande du ".$commande->getDate() ." au prix de : ".htmlspecialchars($commande->getPrix()). " status : ".$commande->getStatus()."</a> <br>";
 
                //echo(
                 //"<a href=index.php?controller=commande&action=Read&id=".rawurlencode($commande->getId()).">"
--- php/view/produit/details.php
+++ php/view/produit/details.php
@@ -2,11 +2,12 @@
     		<?php
     			echo htmlspecialchars($v->getId())."<br>".htmlspecialchars($v->getNom()) ."<br>" . htmlspecialchars($v->getPrix()) . "€ <br> <br>" ;
 
-    			echo '<a href=index.php?action=delete&id='.htmlspecialchars($v->getId()).'>supprimer le produit</a> <br>';
+    			if(Session::is_admin()){
+    			echo '<a href=index.php?action=delete&id='.rawurlencode($v->getId()).'>supprimer le produit</a> <br>';
 
-    			echo '<a href=index.php?action=update&id='.htmlspecialchars($v->getId()).'>mettre à jour le produit </a> <br>';
+    			echo '<a href=index.php?action=update&id='.rawurlencode($v->getId()).'>mettre à jour le produit </a> <br>';
 
-                echo "<a href=index.php?action=panier&id=".htmlspecialchars($v->getId())."> ajouter panier </a> <br>";
+                echo "<a href=index.php?action=panier&id=".rawurlencode($v->getId())."> ajouter panier </a> <br>";}
 
 			?>
 
